32114665583883a559539db47d0f3964cd7d9cdf
Khomechko <gee4697@mail.ru>
2024-05-10 10:42:18 +0700

    feat: props destructuring

--- src/components/user-card/index.jsx
+++ src/components/user-card/index.jsx
@@ -1,13 +1,14 @@
-import React from "react";
 import "./style.scss";
 import { Like } from "../like";
 
-export const UserCard = (props) => {
+export const UserCard = ({ id, title, avatar }) => {
+  // const {id, title, avatar} = props
+
   return (
     <div className="card">
-      <img src={props.avatar} alt="" className="card__avatar" />
-      <h2 className="card__name">{props.title}</h2>
-      <Like />
+      <img src={avatar} alt="" className="card__avatar" />
+      <h2 className="card__name">{title}</h2>
+      <Like id={id} />
     </div>
   );
 };
