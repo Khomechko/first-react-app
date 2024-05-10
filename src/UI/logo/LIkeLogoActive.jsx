e6be0ffa7733e1414dcbdbca758df1b9b6d78914
Khomechko <gee4697@mail.ru>
2024-05-10 10:30:39 +0700

    fix: LikeLogo camelCase warning

--- src/UI/logo/LIkeLogoActive.jsx
+++ src/UI/logo/LIkeLogoActive.jsx
@@ -6,16 +6,16 @@ export const LikeLogoActive = () => {
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
-      xmlnsSketch="http://www.bohemiancoding.com/sketch/ns"
+      xmlnssketch="http://www.bohemiancoding.com/sketch/ns"
       fill="#512689"
       width="16px"
       height="16px"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
@@ -24,15 +24,15 @@ export const LikeLogoActive = () => {
         <g
           id="Page-1"
           stroke="none"
-          stroke-width="1"
+          strokeWidth="1"
           fill="none"
-          fill-rule="evenodd"
-          sketchType="MSPage"
+          fillRule="evenodd"
+          sketchtype="MSPage"
         >
           {" "}
           <g
             id="Icon-Set-Filled"
-            sketchType="MSLayerGroup"
+            sketchtype="MSLayerGroup"
             transform="translate(-102.000000, -882.000000)"
             fill="#512689"
           >
@@ -40,7 +40,7 @@ export const LikeLogoActive = () => {
             <path
               d="M126,882 C122.667,882 119.982,883.842 117.969,886.235 C116.013,883.76 113.333,882 110,882 C105.306,882 102,886.036 102,890.438 C102,892.799 102.967,894.499 104.026,896.097 L116.459,911.003 C117.854,912.312 118.118,912.312 119.513,911.003 L131.974,896.097 C133.22,894.499 134,892.799 134,890.438 C134,886.036 130.694,882 126,882"
               id="heart-like"
-              sketchType="MSShapeGroup"
+              sketchtype="MSShapeGroup"
             >
               {" "}
             </path>{" "}
