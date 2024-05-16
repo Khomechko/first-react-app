cb1f2e1f5e9753113d26aca1088f29ed4a4a1121
Khomechko <gee4697@mail.ru>
2024-05-16 19:31:07 +0700

    feat(css): card changes border color when focused

--- src/components/user-card/index.jsx
+++ src/components/user-card/index.jsx
@@ -6,7 +6,7 @@ export const UserCard = ({ id, title, avatar }) => {
   // const {id, title, avatar} = props
 
   return (
-    <div className="card">
+    <div className="card" tabindex="0">
       <Link to={`/partner/${id}`}>
         <img src={avatar} alt="" className="card__avatar" />
       </Link>
