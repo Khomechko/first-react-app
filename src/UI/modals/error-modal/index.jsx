4162a202dd6d847799e7e091ad6764cd3a514369
Khomechko <gee4697@mail.ru>
2024-06-05 19:51:58 +0700

    refactor: style logic

--- src/UI/modals/error-modal/index.jsx
+++ src/UI/modals/error-modal/index.jsx
@@ -2,14 +2,11 @@ import "./style.scss";
 import React from "react";
 
 export const ErrorModal = ({ children, visible, setVisible }) => {
-  const rootClasses = ["error-modal"];
-
-  if (visible) {
-    rootClasses.push("active");
-  }
-
   return (
-    <div className={rootClasses.join(" ")} onClick={() => setVisible(false)}>
+    <div
+      className={`error-modal${visible ? " active" : ""}`}
+      onClick={() => setVisible(false)}
+    >
       <div
         className="error-modal__content"
         onClick={(e) => e.stopPropagation()}
