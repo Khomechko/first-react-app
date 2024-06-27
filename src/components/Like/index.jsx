892015afaf0b87ecb4c21049d1c2b236329c15e8
Khomechko <gee4697@mail.ru>
2024-06-27 18:57:56 +0700

    fix: removed useBoolState hook

--- src/components/Like/index.jsx
+++ src/components/Like/index.jsx
@@ -1,28 +1,27 @@
 import { LikeLogoActive } from "../../ui/logo/LIkeLogoActive";
 import { LikeLogo } from "../../ui/logo/LikeLogo";
-import { useEffect } from "react";
-import { useBoolState } from "../../hooks/useBoolState";
+import { useEffect, useState } from "react";
 
 export const Like = (props) => {
-  const { value, setValue } = useBoolState(false);
+  const [active, setActive] = useState(false);
 
   const toLike = () => {
-    setValue((prev) => !prev);
+    setActive((prev) => !prev);
   };
 
   useEffect(() => {
     const raw = localStorage.getItem(props.id);
-    setValue(JSON.parse(raw));
+    setActive(JSON.parse(raw));
   }, []);
 
   useEffect(() => {
     localStorage.setItem(props.id, value);
-  }, [value]);
+  }, [active]);
 
   return (
     <div className="like">
       <div className="like-box" onClick={toLike}>
-        {value ? <LikeLogoActive /> : <LikeLogo />}
+        {active ? <LikeLogoActive /> : <LikeLogo />}
       </div>
     </div>
   );
