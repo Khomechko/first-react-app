b0ac614f5759c2697eba4b54b44bd125bba28001
mikhail <gee4697@mail.ru>
2024-09-22 18:48:00 +0700

    feat: added memoized selector

--- src/UI/pagination/index.tsx
+++ src/UI/pagination/index.tsx
@@ -1,16 +1,18 @@
-import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { useAppDispatch } from "../../hooks/reduxHooks";
 import { fetchUsers } from "../../services/fetch-users";
 import { setUsersToDefault } from "../../store/reducers/userSlice";
 import "./style.scss";
+import { store } from "../../store";
+import { selectPageAndTotalPage } from "../../store/reducers/userSlice/selectors";
 
 export const DEFAULT_NUMBER_OF_PAGE = 1;
 
 export const Pagination = () => {
   const dispatch = useAppDispatch();
-  const { numberOfPage, numberOfTotalPage } = useAppSelector((state) => ({
-    numberOfPage: state.user.page,
-    numberOfTotalPage: state.user.totalPages,
-  }));
+
+  const { numberOfPage, numberOfTotalPage } = selectPageAndTotalPage(
+    store.getState()
+  );
 
   return (
     <div className="pagination">
