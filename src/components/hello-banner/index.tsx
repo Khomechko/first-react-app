b95b2322f4a6f87806823f978bdb3ba03f58a080
mikhail <gee4697@mail.ru>
2024-10-04 12:35:27 +0700

    feat: CSS-Modules added in hello-banner

--- src/components/hello-banner/index.tsx
+++ src/components/hello-banner/index.tsx
@@ -1,10 +1,10 @@
-import "./style.scss";
+import style from "./style.module.scss";
 
 export const HelloBanner = () => {
   return (
-    <div className="hello-banner">
+    <div className={style.helloBanner}>
       <h1 className="title">Наша команда</h1>
-      <h2 className="hello-banner__subtitle">
+      <h2 className={style.subtitle}>
         Это опытные специалисты, хорошо разбирающиеся во всех задачах, которые
         ложатся на их плечи, и умеющие находить выход из любых, даже самых
         сложных ситуаций.
